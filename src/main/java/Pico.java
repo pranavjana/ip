eb07fcb1a16558c4c65c41e6c5cfe2359a125c98
Pranav Janakiraman <pranavjana2003@gmail.com>
2026-02-05 16:57:18 +0800

    Level-1: Add echo feature with chat interface

--- src/main/java/Pico.java
+++ src/main/java/Pico.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Pico {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println(line);
-        System.out.println(" Hello! I'm Pico");
-        System.out.println(" What can I do for you?");
+        System.out.println(" Greetings! I'm Pico, your extraterrestrial bot!");
+        System.out.println(" What can I do for you, earthling?");
         System.out.println(line);
 
+        System.out.print("                              You: ");
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("Pico: " + input);
+            System.out.println();
+            System.out.print("                              You: ");
+            input = scanner.nextLine();
+        }
+
+        System.out.println(line);
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println(line);
+
+        scanner.close();
     }
 }
