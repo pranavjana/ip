1b5474b4d428c9ce2c835e1419592a37b53f14e5
Pranav Janakiraman <pranavjana2003@gmail.com>
2026-02-05 16:30:04 +0800

    Level-0: Rename chatbot to Pico with greet and exit
    - Rename Duke to Pico
    - Add greeting and exit messages
    - Add *.class to .gitignore

---
+++ src/main/java/Pico.java
@@ -0,0 +1,13 @@
+public class Pico {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(" Hello! I'm Pico");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
